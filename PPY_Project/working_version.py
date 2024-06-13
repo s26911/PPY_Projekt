93eb3d4eb9d5ba4b214cb6c02408a302b11e63e9
Jakub Gołębiewski <s26911@pjwstk.edu.pl>
2024-06-14 01:05:56 +0200

    Ships can now be placed on the board

--- PPY_Project/working_version.py
+++ PPY_Project/working_version.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter.ttk import Combobox
+import copy
 
 
 class BattleshipsGame:
@@ -8,6 +9,7 @@ class BattleshipsGame:
         self.pvp = True
         self.board_size = 10
         self.fleet = {5: 1, 4: 1, 3: 2, 2: 1}
+        self.game_board = [[]]  # 2-dimensional int array, 0:empty, 1:ship, 2:sunken
 
     def update_board_size(self, size):
         self.board_size = int(size)
@@ -36,23 +38,37 @@ class BattleshipsGame:
                 messagebox.showerror("Error", "Cannot remove ship not present in fleet.")
             elif self.fleet[size] - quantity < 0:
                 messagebox.showerror("Error", "Cannot remove more ships than are absent in fleet.")
+            elif len(self.fleet) == 1 and self.fleet[size] - quantity <= 0:
+                messagebox.showerror("Error", "Fleet cannot be empty.")
             else:
                 self.fleet[size] -= quantity
                 if self.fleet[size] == 0:
                     self.fleet.pop(size)
 
-    def get_fleet_info(self):
+    def get_fleet_info(self, fleet=None):
+        if fleet is None:
+            fleet = self.fleet
         string = ""
-        for ship_size in sorted(self.fleet.keys(), reverse=True):
-            string += "Size: {}, quantity: {}\n".format(ship_size, self.fleet[ship_size])
+        for ship_size in sorted(fleet.keys(), reverse=True):
+            string += "Size: {}, quantity: {}\n".format(ship_size, fleet[ship_size])
         return string
+
+    def add_ship_to_board(self, row, column, ship_size, orientation):
+        if orientation == "horizontal":
+            for i in range(ship_size):
+                self.game_board[row][column + i] = 1
+        else:
+            for i in range(ship_size):
+                self.game_board[row + i][column] = 1
 
 
 class GUI(Tk):
     def __init__(self, game):
         super().__init__()
-        self.v = StringVar()
         self.game = game
+        self.ship_size = IntVar()
+        self.orientation = "horizontal"
+        self.available_fleet = {}
         self.title("Battleships")
         self.start_menu_ui()
 
@@ -115,7 +131,8 @@ class GUI(Tk):
 
         # buttons to proceed
         proceed_buttons_cont = Frame(self)
-        next_button = Button(proceed_buttons_cont, text="Next", command=lambda: self.prepare_battleship_board_ui(self.game.board_size))
+        next_button = Button(proceed_buttons_cont, text="Next",
+                             command=lambda: self.prepare_battleship_board_ui(self.game.board_size))
         back_button = Button(proceed_buttons_cont, text="Back", command=lambda: self.start_menu_ui())
         (Label(proceed_buttons_cont, text="\nProceed to placing ships or return to main menu")
          .grid(row=0, column=1, columnspan=2))
@@ -130,6 +147,8 @@ class GUI(Tk):
     def prepare_battleship_board_ui(self, board_size):
         self.clear_frame()
         self.geometry("")
+        self.game.game_board = [[0 for _ in range(board_size)] for _ in range(board_size)]
+        self.available_fleet = copy.copy(self.game.fleet)  # fleet available to place on the board
 
         # fleet info
         ships_string = "Ships remaining:\n" + self.game.get_fleet_info()
@@ -139,7 +158,7 @@ class GUI(Tk):
         # canvas representing the game board
         cols = board_size
         rows = board_size
-        cell_size = int(400/board_size)
+        cell_size = int(400 / board_size)
         canvas = Canvas(self, width=cols * cell_size, height=rows * cell_size)
         canvas.pack()
 
@@ -154,14 +173,17 @@ class GUI(Tk):
                 rect = canvas.create_rectangle(x1, y1, x2, y2, fill='white', outline='black')
                 row_cells.append(rect)
             board.append(row_cells)
-        ship_size = IntVar(value=3)
+
+        (canvas.bind("<Button-1>", lambda event:
+        self.place_ship(event, canvas, cell_size, board, self.ship_size.get(), self.orientation, ships_label, ship_size_selector)))
 
         # select ship to place
         ship_selector_cont = Frame(self)
         Label(ship_selector_cont, text="Select ship size:").grid(row=0, column=0)
-        ship_size_selector = Combobox(ship_selector_cont, textvariable=ship_size,
+        ship_size_selector = Combobox(ship_selector_cont, textvariable=self.ship_size,
                                       values=list(sorted(self.game.fleet.keys(), reverse=True)))
         ship_size_selector.grid(row=0, column=1)
+        self.ship_size.set(int(ship_size_selector["values"][0]))
         ship_selector_cont.pack()
 
         # navigation buttons
@@ -179,6 +201,47 @@ class GUI(Tk):
         next_button.grid(row=1, column=3, sticky=E)
         proceed_buttons_cont.pack()
 
+    def place_ship(self, event, canvas, cell_size, board, ship_size, orientation, ships_label, ship_size_selector):
+        col = event.x // cell_size
+        row = event.y // cell_size
+        if self.can_place_ship(canvas, row, col, ship_size, board, orientation):
+            self.game.add_ship_to_board(row, col, ship_size, orientation)
+            self.update_available_ships(ship_size)
+
+            ship_size_selector.config(value=list(sorted(self.available_fleet.keys(), reverse=True)))
+            if len(self.available_fleet) == 0:
+                ships_label.config(text="All ships placed. Click Next...")
+            else:
+                ship_size_selector.current(0)
+                ships_label.config(text="Ships remaining:\n" + self.game.get_fleet_info(self.available_fleet))
+            for i in range(ship_size):
+                if orientation == "horizontal":
+                    item = board[row][col + i]
+                else:
+                    item = board[row + i][col]
+                canvas.itemconfig(item, fill='black')
+
+            #     hover_state["clicked"][(row, col + i) if orientation[0] == "horizontal" else (row + i, col)] = True
+            # ships.append((row, col, ship_size, orientation[0]))
+
+    def can_place_ship(self, canvas, row, col, ship_size, board, orientation):
+        if len(self.available_fleet) == 0:
+            return False
+        for i in range(ship_size):
+            if orientation == "horizontal":
+                if col + i >= len(board[row]) or canvas.itemcget(board[row][col + i], 'fill') == 'black':
+                    return False
+            else:
+                if row + i >= len(board) or canvas.itemcget(board[row + i][col], 'fill') == 'black':
+                    return False
+        return True
+
+    def update_available_ships(self, ship_size):
+        qty = self.available_fleet[ship_size]
+        if qty > 1:
+            self.available_fleet[ship_size] = qty - 1
+        else:
+            self.available_fleet.pop(ship_size)
 
 if __name__ == "__main__":
     game = BattleshipsGame()
